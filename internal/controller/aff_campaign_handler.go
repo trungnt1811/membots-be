6af111898c6e979270e6f833f67c39a565f5026b
trung <trung.nguyen25@tiki.vn>
2023-09-10 20:41:51 +0700

    add GetAffCampaignByAccesstradeId handler

--- internal/controller/aff_campaign_handler.go
+++ internal/controller/aff_campaign_handler.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/astraprotocol/affiliate-system/internal/interfaces"
 	"github.com/astraprotocol/affiliate-system/internal/util"
@@ -37,6 +38,33 @@ func (handler *AffCampaignHandler) GetAllAffCampaign(ctx *gin.Context) {
 	size := ctx.GetInt("size")
 
 	response, err := handler.AffCampaignService.GetAllAffCampaign(ctx, page, size)
+	if err != nil {
+		util.RespondError(ctx, http.StatusInternalServerError, "Get list of all aff campaign error: ", err)
+		return
+	}
+	ctx.JSON(http.StatusOK, response)
+}
+
+// GetAffCampaignByAccesstradeId Get aff campaign by accesstrade id
+// @Summary Get aff campaign by accesstrade id
+// @Description Get aff campaign by accesstrade id
+// @Tags 	app
+// @Accept	json
+// @Produce json
+// @Param accesstradeId query string false "accesstradeId to query"
+// @Success 200 		{object}	dto.AffCampaignAppDto
+// @Failure 401 		{object}	dto.GeneralError
+// @Failure 400 		{object}	dto.GeneralError
+// @Router 	/api/v1/app//aff-campaign [get]
+func (handler *AffCampaignHandler) GetAffCampaignByAccesstradeId(ctx *gin.Context) {
+	accesstrade := ctx.DefaultQuery("accesstradeId", "")
+	accesstradeId, err := strconv.ParseUint(accesstrade, 10, 64)
+	if err != nil {
+		util.RespondError(ctx, http.StatusBadRequest, "get accesstradeId param error: ", err)
+		return
+	}
+
+	response, err := handler.AffCampaignService.GetAffCampaignByAccesstradeId(ctx, accesstradeId)
 	if err != nil {
 		util.RespondError(ctx, http.StatusInternalServerError, "Get list of all aff campaign error: ", err)
 		return

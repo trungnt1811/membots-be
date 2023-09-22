cbd3857ea0e66e8d9a2deab0a2870c8f5f92fb12
KienLe <kienlt@stellalab.com>
2023-09-22 16:41:34 +0700

    doc(reward): order api use reward tags

--- test/order_approved_test.go
+++ test/order_approved_test.go
@@ -15,7 +15,7 @@ func TestProduceOrderApprovedMsg(t *testing.T) {
 	producer := msgqueue.NewKafkaProducer(msgqueue.KAFKA_TOPIC_AFF_ORDER_APPROVE)
 
 	msg := msgqueue.MsgOrderApproved{
-		AtOrderID: "185857576",
+		AtOrderID: "230915CPRGY3A5AKHANH",
 	}
 
 	b, err := json.Marshal(msg)
@@ -26,5 +26,5 @@ func TestProduceOrderApprovedMsg(t *testing.T) {
 	})
 	assert.Nil(t, err)
 
-	fmt.Println("Push MsgOrderApproved...")
+	fmt.Println("Push MsgOrderApproved..")
 }

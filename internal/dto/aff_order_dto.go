a8ea147d566de0fb0c8051e26bcbf55b857710eb
KienLe <kienlt@stellalab.com>
2023-09-25 21:03:12 +0700

    fix(order): order history return null data

--- internal/dto/aff_order_dto.go
+++ internal/dto/aff_order_dto.go
@@ -23,7 +23,7 @@ type OrderDetailsDto struct {
 	CommissionFee                  float64   `json:"commission_fee"`                            // commission fee (in percentage)
 	BuyTime                        time.Time `json:"sales_time,omitempty"`                      // dat hang thanh cong
 	ConfirmedTime                  time.Time `json:"confirmed_time,omitempty"`                  // don hang duoc ghi nhan
-	RejectedTime                   time.Time `json:"rejected_time,omitempty"`                   // don hang duoc ghi nhan
+	RejectedTime                   time.Time `json:"rejected_time,omitempty"`                   // don hang bi huy
 	RewardFirstPartReleasedTime    time.Time `json:"reward_first_part_released_time,omitempty"` // xac nhan hoan tat nhan 50%
 	RewardFirstPartReleasedAmount  float64   `json:"reward_first_part_released_amount,omitempty"`
 	RewardSecondPartUnlockedAmount float64   `json:"reward_second_part_unlocked_amount,omitempty"`

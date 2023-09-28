9361a6ad3387970ac9ba123c8502eb8f645718cb
Nhan Hoang <siege.250294@gmail.com>
2023-09-28 11:37:06 +0700

    fix: status rewarding instead of approved

--- internal/app/order/order_ucase.go
+++ internal/app/order/order_ucase.go
@@ -221,7 +221,7 @@ func (u *orderUCase) CheckOrderConfirmed() (int, error) {
 	t := time.Now()
 	q := map[string]any{
 		"is_confirmed": 0,
-		"order_status": model.OrderStatusApproved,
+		"order_status": model.OrderStatusRewarding,
 	}
 	orders, err := u.Repo.QueryOrdersConfirmedBefore(t, q)
 	if err != nil {

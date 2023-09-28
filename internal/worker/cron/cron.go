527f777754af32034588477eefd539f3c830fcef
Nhan Hoang <siege.250294@gmail.com>
2023-09-28 08:27:15 +0700

    update: uncomment missing cron

--- internal/worker/cron/cron.go
+++ internal/worker/cron/cron.go
@@ -2,6 +2,7 @@ package cron
 
 import (
 	"github.com/astraprotocol/affiliate-system/conf"
+	"github.com/astraprotocol/affiliate-system/internal/app/campaign"
 	"github.com/astraprotocol/affiliate-system/internal/app/order"
 	"github.com/astraprotocol/affiliate-system/internal/infra/accesstrade"
 	"github.com/astraprotocol/affiliate-system/internal/infra/msgqueue"
@@ -12,19 +13,19 @@ func RegisterCronJobs(config *conf.Configuration, db *gorm.DB) {
 	rdc := conf.RedisConn()
 
 	// Initialize dependencies
-	// campRepo := campaign.NewCampaignRepository(db)
+	campRepo := campaign.NewCampaignRepository(db)
 	atRepository := accesstrade.NewAccessTradeRepository(config.AccessTradeAPIKey, 3, 30)
-	// atUCase := accesstrade.NewAccessTradeUCase(atRepository, campRepo)
+	atUCase := accesstrade.NewAccessTradeUCase(atRepository, campRepo)
 
 	orderRepo := order.NewOrderRepository(db)
 	orderUCase := order.NewOrderUCase(orderRepo, atRepository, msgqueue.NewKafkaProducer(msgqueue.KAFKA_TOPIC_AFF_ORDER_UPDATE))
 
 	// Initialize workers
-	// atWorker := NewAccessTradeWorker(rdc, atUCase)
-	// go func() {
-	// 	// Run brand's campaigns sync
-	// 	atWorker.RunJob()
-	// }()
+	atWorker := NewAccessTradeWorker(rdc, atUCase)
+	go func() {
+		// Run brand's campaigns sync
+		atWorker.RunJob()
+	}()
 
 	orderConfirmedWorker := NewOrderConfirmedWorker(rdc, orderUCase)
 	go func() {

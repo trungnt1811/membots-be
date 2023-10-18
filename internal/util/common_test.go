cd9148d189644ea3b9d67123bd1fe4d562932cbf
KienLe <kienlt@stellalab.com>
2023-10-18 09:59:16 +0700

    fix(util): test format noti amount

--- internal/util/common_test.go
+++ internal/util/common_test.go
@@ -1,12 +1,15 @@
 package util
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFormatNotiAmount(t *testing.T) {
-	number := 12435475679.12921
-	out := FormatNotiAmt(number)
-	fmt.Println(out)
+	nums := []float64{12435475679.12921, 312372137, 234324.23, 43.2}
+	expectResults := []string{"12,435,475,679.13", "312,372,137.00", "234,324.23", "43.20"}
+	for idx, n := range nums {
+		assert.Equal(t, expectResults[idx], FormatNotiAmt(n))
+	}
 }

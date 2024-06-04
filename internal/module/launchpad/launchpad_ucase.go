4cc8ff2d89f322552b158282948b41d728a15a58
Khanh Hoa <khanhoait.bka@gmail.com>
2024-06-04 18:04:40 +0700

    feat: add launchpad api

---
+++ internal/module/launchpad/launchpad_ucase.go
@@ -0,0 +1,55 @@
+package launchpad
+
+import (
+	"context"
+	"github.com/flexstack.ai/membots-be/internal/interfaces"
+	"strconv"
+
+	unigraphclient "github.com/emersonmacro/go-uniswap-subgraph-client"
+	"github.com/flexstack.ai/membots-be/internal/dto"
+)
+
+type launchpadUCase struct {
+	Client *unigraphclient.Client
+}
+
+func NewLaunchpadUcase(client *unigraphclient.Client) interfaces.LaunchpadUCase {
+	return &launchpadUCase{Client: client}
+}
+
+func (uc *launchpadUCase) GetHistory(ctx context.Context, address string) (dto.LaunchpadInfoRsp, error) {
+	requestOpts := &unigraphclient.RequestOptions{
+		IncludeFields: []string{
+			"*",
+		},
+	}
+
+	response, err := uc.Client.GetSwapHistoryByMemeToken(ctx, address, requestOpts)
+	if err != nil {
+		return dto.LaunchpadInfoRsp{}, err
+	}
+	// convert response to dto.SwapHistoryByAddressRsp
+	var transactions []dto.Transaction
+	for _, meme := range response.MemecoinExits {
+		timestamp, err := strconv.ParseUint(meme.BlockTimestamp, 10, 64)
+		if err != nil {
+			timestamp = 0
+		}
+		transactions = append(transactions, dto.Transaction{
+			AmountETH:     meme.AmountETH,
+			AmountMeme:    meme.AmountMeme,
+			WalletAddress: meme.User,
+			TxHash:        meme.TransactionHash,
+			TxType:        "BUY",
+			MemeID:        meme.ID,
+			Epoch:         timestamp,
+		})
+	}
+
+	return dto.LaunchpadInfoRsp{LaunchpadInfo: dto.LaunchpadInfo{
+		Transactions: transactions,
+		Status:       "",
+		TargetETH:    "",
+		CollectedETH: "",
+	}}, nil
+}

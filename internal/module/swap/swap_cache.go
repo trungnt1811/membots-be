7c4b9bcda9154e65b5fdd237bcaceeeb917c2291
Khanh Hoa <khanhoait.bka@gmail.com>
2024-06-04 15:15:19 +0700

    feat: add swap cache

--- internal/module/swap/swap_cache.go
+++ internal/module/swap/swap_cache.go
@@ -1,1 +1,46 @@
 package swap
+
+import (
+	"context"
+	"fmt"
+	"github.com/flexstack.ai/membots-be/internal/dto"
+	"github.com/flexstack.ai/membots-be/internal/infra/caching"
+	"github.com/flexstack.ai/membots-be/internal/interfaces"
+	"time"
+)
+
+const (
+	keyPrefixSwap = "swap_"
+	cacheTimeSwap = 5 * time.Second
+)
+
+type swapCache struct {
+	SwapUCase interfaces.SwapUCase
+	Cache     caching.Repository
+}
+
+func NewSwapCacheRepository(repo interfaces.SwapUCase,
+	cache caching.Repository,
+) interfaces.SwapUCase {
+	return &swapCache{
+		SwapUCase: repo,
+		Cache:     cache,
+	}
+}
+
+func (s *swapCache) GetSwaps(ctx context.Context, address string) (dto.SwapHistoryByAddressRsp, error) {
+	key := &caching.Keyer{Raw: keyPrefixSwap + fmt.Sprint("GetSwaps_", address)}
+	var swaps dto.SwapHistoryByAddressRsp
+	err := s.Cache.RetrieveItem(key, &swaps)
+	if err != nil {
+		// cache miss
+		swaps, err = s.GetSwaps(ctx, address)
+		if err != nil {
+			return swaps, err
+		}
+		if err = s.Cache.SaveItem(key, swaps, cacheTimeSwap); err != nil {
+			return swaps, err
+		}
+	}
+	return swaps, nil
+}

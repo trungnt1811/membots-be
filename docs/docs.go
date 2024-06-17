f3e31bb9b96f38396538961e1ae14a966a703f3d
trung <trungqn.ptit@gmail.com>
2024-06-17 12:24:26 +0700

    chore: update docs

--- docs/docs.go
+++ docs/docs.go
@@ -406,7 +406,7 @@ const docTemplate = `{
                     }
                 },
                 "txCounter": {
-                    "type": "string"
+                    "type": "integer"
                 }
             }
         },
